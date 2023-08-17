29daa0b61945bac554fcb31944111499818feeb2
Gavi Salcedo <gavi@Gavis-MacBook-Air.local>
2023-08-17 12:05:35 +0800

    refactor(TailWind-CSS): redirected globals.css to app directory
    setup tailwindcss in project

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function HomePage() {
-  return <div>This is my new App Route Home Page</div>;
+  return (
+    <div className="text-3xl font-bold underline">
+      This is my new App Route Home Page
+    </div>
+  );
 }
