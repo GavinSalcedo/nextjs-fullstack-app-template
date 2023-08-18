6c5b23c4d67d858f62d10e31dc1ad7dd5c6e2fee
Gavi Salcedo <gavi@Gavis-MacBook-Air.local>
2023-08-18 14:14:57 +0800

    fix(Cart): fix fixed position of the itemCount indicator in Cart component

--- app/_components/Cart/index.tsx
+++ app/_components/Cart/index.tsx
@@ -5,9 +5,9 @@ interface ICart {
 function Cart(props: ICart) {
   const { itemCount = 0 } = props;
   return (
-    <span className="inline-block">
+    <span>
       {itemCount !== null && itemCount > 0 ? (
-        <div className="bg-red-700 rounded-full text-center fixed w-6 h-6 top-5 right-2.5">
+        <div className="absolute bg-red-700 rounded-full text-center w-6 h-6 top-5 right-2.5">
           {itemCount}
         </div>
       ) : null}
