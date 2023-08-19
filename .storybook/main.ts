064ba7e625d9c548ad314707439ee334d3fc21bd
Gavi Salcedo <gavi@Gavis-MacBook-Air.local>
2023-08-19 11:06:03 +0800

    feat(Storybook): add storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,14 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
   ],
   framework: {
