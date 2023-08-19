5928f8088438516ddabe0bdfe7d2d6c4fe007020
Gavi Salcedo <gavi@Gavis-MacBook-Air.local>
2023-08-19 10:46:47 +0800

    refactor(NextConfig): update hostname to accept all

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'img.represent.com',
+        hostname: '**',
         port: '',
         pathname: '/**',
       },
